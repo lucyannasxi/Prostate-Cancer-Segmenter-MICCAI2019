b90934890eb7615884ec302d21b720e2b4422945
lucyannasxi <lucyanguo@proton.me>
2022-10-04 22:04:34 +0000

    tweak build

--- generate_labels.py
+++ generate_labels.py
@@ -3,4 +3,5 @@ from utils import *
 # fill your path from your local folder path here
 root_path = './MICCAI_2019_pathology_challenge/'
 path_to_save_labels = './labels'
-make_dirs(path_to_save_l
+make_dirs(path_to_save_labels)
+
