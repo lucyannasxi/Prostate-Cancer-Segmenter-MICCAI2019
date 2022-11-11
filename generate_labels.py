15189aebab9cc97c77953206a3b5988505e55abd
lucyannasxi <lucyanguo@proton.me>
2022-11-11 14:28:01 +0000

    Remove extra semicolons

--- generate_labels.py
+++ generate_labels.py
@@ -11,4 +11,7 @@ img_paths = sorted(glob.glob(os.path.join(root_path,'Train Imgs/Train Imgs/*')))
 print('Imgs found:', len(img_paths))
 assert len(img_paths)==244 , 'Check your path'
 # Majority voting and save image label
-preprocess_labels(maps, img_paths, path_to_save_l
+preprocess_labels(maps, img_paths, path_to_save_labels)
+
+
+
