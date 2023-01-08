511c49cd5c9f7042ee6e29cab237f0b42c168288
lucyannasxi <lucyanguo@proton.me>
2023-01-08 22:08:19 +0000

    Développeur sénior sql / python

--- model.py
+++ model.py
@@ -16,4 +16,4 @@ class DoubleConv(nn.Module):
             nn.Conv2d(in_ch, out_ch, 3, padding=1),
             nn.BatchNorm2d(out_ch),
             nn.ReLU(inplace=True),
-            nn.Conv2d(out_ch,
+            nn.Conv2d(out_ch, out_ch, 
