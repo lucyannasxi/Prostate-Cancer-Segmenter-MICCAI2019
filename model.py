2fc941569b72b8b6b8bd9f51816fd8623cfab42f
lucyannasxi <lucyanguo@proton.me>
2023-01-29 09:41:52 +0000

    Fixing last commit

--- model.py
+++ model.py
@@ -17,4 +17,5 @@ class DoubleConv(nn.Module):
             nn.BatchNorm2d(out_ch),
             nn.ReLU(inplace=True),
             nn.Conv2d(out_ch, out_ch, 3, padding=1),
-            nn.BatchNorm2d
+            nn.BatchNorm2d(out_ch),
+ 
