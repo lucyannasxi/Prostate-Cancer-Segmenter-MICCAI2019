c511960e5b08498d23264a36054d3e903935b934
lucyannasxi <lucyanguo@proton.me>
2023-02-22 18:47:20 +0000

    chore(deps): update dependency eslint to v5.12.1

--- model.py
+++ model.py
@@ -28,4 +28,4 @@ class DoubleConv(nn.Module):
 class InConv(nn.Module):
     def __init__(self, in_ch, out_ch):
         super(InConv, self).__init__()
-        self.conv = D
+        self.conv = DoubleCon
