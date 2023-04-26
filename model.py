e486d39b1fce367c8126ccac2363057da5c4dc16
lucyannasxi <lucyanguo@proton.me>
2023-04-26 14:17:40 +0000

    Renamed README file

--- model.py
+++ model.py
@@ -52,4 +52,4 @@ class Up(nn.Module):
         super(Up, self).__init__()
 
         if bilinear:
-            self.up = nn.Upsample(scale_factor=2, mode='bi
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', ali
