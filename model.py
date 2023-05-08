12c21510a98b5330dbf1050b0e54fb78809525a0
lucyannasxi <lucyanguo@proton.me>
2023-05-08 14:35:22 +0000

    Fix merge errors.

--- model.py
+++ model.py
@@ -54,4 +54,4 @@ class Up(nn.Module):
         if bilinear:
             self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
         else:
-            self.up = nn.ConvTranspose2d(in_ch 
+            self.up = nn.ConvTranspose2d(in_ch // 2, in_c
