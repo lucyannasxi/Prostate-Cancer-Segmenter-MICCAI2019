9f217307c4cd220c575c5e0260dcbb8200b68a04
lucyannasxi <lucyanguo@proton.me>
2023-08-06 09:56:55 +0000

    Updating packages

--- model.py
+++ model.py
@@ -86,4 +86,4 @@ class Unet(pl.LightningModule):
         super(Unet, self).__init__()
         self.criterion = DiceLoss()
         self.inc = InConv(n_channels, 64)
-        self.down1 = D
+        self.down1 = Down(64
