86df304d72679db76797b3866220b9cf6e83d6b5
lucyannasxi <lucyanguo@proton.me>
2023-12-03 15:35:47 +0000

    bench

--- model.py
+++ model.py
@@ -101,4 +101,5 @@ class Unet(pl.LightningModule):
         x2 = self.down1(x1)
         x3 = self.down2(x2)
         x4 = self.down3(x3)
-        x5 = self.down4(x
+        x5 = self.down4(x4)
+      
