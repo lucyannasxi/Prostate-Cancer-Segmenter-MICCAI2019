841f66c5284df2166770f931308baa8a4d76725d
lucyannasxi <lucyanguo@proton.me>
2024-04-20 21:57:02 +0000

    Update proguard rules

--- train.py
+++ train.py
@@ -8,3 +8,11 @@ from torch.utils.data import DataLoader
 
 from dataloader import Gleason2019SaveDISK
 from model import Unet
+from utils import shuffle_lists
+
+# Data preparation
+generate_sub_images = True
+root_path = './MICCAI_2019_pathology_challenge/'
+folder_to_save_train_samples = './train_samples'
+folder_to_save_val_samples = './val_samples'
+train_imgs = sorted(glob.glob(os.path.join(root_path, 'Train Imgs/Train Imgs/*.jpg')))
