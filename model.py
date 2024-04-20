cff55d20c5d73430e788012e56d188a7bc704771
lucyannasxi <lucyanguo@proton.me>
2024-04-20 06:07:52 +0000

    add a few comments

--- model.py
+++ model.py
@@ -126,4 +126,5 @@ class Unet(pl.LightningModule):
         return loss
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), l
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-4)
+    
