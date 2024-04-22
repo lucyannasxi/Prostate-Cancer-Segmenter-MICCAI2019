5e00c881338bedfdc4f8c6641e9adb93e2ee9523
lucyannasxi <lucyanguo@proton.me>
2024-04-22 13:07:42 +0000

    Update MainActivity.java

--- train.py
+++ train.py
@@ -16,3 +16,15 @@ root_path = './MICCAI_2019_pathology_challenge/'
 folder_to_save_train_samples = './train_samples'
 folder_to_save_val_samples = './val_samples'
 train_imgs = sorted(glob.glob(os.path.join(root_path, 'Train Imgs/Train Imgs/*.jpg')))
+
+# You have to generate the labels first by running the script
+labels_final = sorted(glob.glob('./labels/*.png'))
+assert len(labels_final) == len(train_imgs)
+train_imgs, labels_final = shuffle_lists(train_imgs, labels_final)
+val_loader = Gleason2019SaveDISK('val', train_imgs, labels_final, (0.8, 0.2),
+                                 (512, 512), samples=10)
+train_loader = Gleason2019SaveDISK('train', train_imgs, labels_final, (0.8, 0.2),
+                                   (512, 512), samples=40)
+if generate_sub_images:
+    val_loader.generate_data(folder_to_save_val_samples)
+    train_loader.generate_data(folder_to_save_train_samples)
