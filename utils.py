8bcb432fb54b2c3a6e1e7ee2cf61beed4a196b46
lucyannasxi <lucyanguo@proton.me>
2024-04-24 20:19:45 +0000

    feat: update README.md

--- utils.py
+++ utils.py
@@ -6,3 +6,15 @@ import shutil
 import time
 
 import imageio
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def make_dirs(path):
+    if os.path.exists(path):
+        shutil.rmtree(path)
+        os.mkdir(path)
+    else:
+        os.makedirs(path)
